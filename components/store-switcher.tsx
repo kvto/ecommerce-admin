0656fa46677f37fbc94c69c28cbb8ae733e0b24b
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-07-17 10:38:40 -0500

    Settings page

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -2,11 +2,14 @@
 
 import { Store } from "@prisma/client";
 
-import { PopoverTrigger } from "@/components/ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { useParams, useRouter } from "next/navigation";
-import { StethoscopeIcon } from "lucide-react";
+import { Check, ChevronsUpDown, PlusCircle, Store as StoreIcon} from "lucide-react";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from "@/components/ui/command";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
@@ -36,8 +39,61 @@ export default function StoreSwitcher({
         router.push(`/${store.value}`);
     }
     return(
-        <div>
-            Store Switcher
-        </div>
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                variant="outline"
+                size="sm"
+                role="combobox"
+                aria-expanded={open}
+                aria-label="Eligir una tienda"
+                className={cn("w-[200px] justify-between", className)}>
+                    
+                    <StoreIcon className="mr-2 h-4 w-4"/>
+                    {currentStore?.label}
+                    <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandList>
+                        <CommandInput placeholder="Buscar tienda..."/>
+                        <CommandEmpty>Tienda no encontrada</CommandEmpty>
+                        <CommandGroup heading="Tiendas">
+                            {formattedItems.map((store) => (
+                                <CommandItem
+                                key={store.value}
+                                onSelect={()=> onStoreSelect(store)}
+                                className="text-sm">
+                                    <StoreIcon className="mr-2 h-4 w-4"/>
+                                    {store.label}
+                                    <Check
+                                    className={cn(
+                                        "ml-auto h-4 w-4",
+                                        currentStore?.value === store.value
+                                        ? "opacity-100"
+                                        : "opacity-0"
+                                        )} />
+                                </CommandItem>
+                            ))}
+
+                        </CommandGroup>
+                    </CommandList>
+                    <CommandSeparator />
+                    <CommandList>
+                        <CommandGroup>
+                            <CommandItem
+                            onSelect={() => {
+                                setOpen(false);
+                                storeModal.onOpen();
+                            }}>
+                                <PlusCircle className="mr-2 h-5 w-5"/>
+                                Crear tienda
+                            </CommandItem>
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+        </Popover>
     )
 }
